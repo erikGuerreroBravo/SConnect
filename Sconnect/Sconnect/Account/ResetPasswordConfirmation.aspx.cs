b2229857b7f50a9ca0338378083ded56fe2c6cbb
erikg <erikg@DESKTOP-JH1G86M>
2016-08-30 17:31:03 -0500

    inicializacion

---
+++ Sconnect/Sconnect/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Sconnect.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Sconnect/Sconnect/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sconnect.Startup))]
+namespace Sconnect
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
